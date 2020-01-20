cda419a5acaf12367a543819c4c76fc41a0744cd
Macz309 <leugim98zurc@gmail.com>
2020-01-19 22:29:35 -0600

    version2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 
 //Variables Globales.
 var numHorarios=0
+var numNotacion=0;
 crearHorario()
 
 
@@ -23,15 +24,16 @@ function crearHorario(){
         horario+= `<div class="materias">
                     <p class="hora">${r}:00 - ${r+1}:00</p>`;
         for (let c = 0; c < 6; c++) {
-            horario+=`<input class="materia" id="${numHorarios}${r}${c}" autocomplete="off"> `;           
+            horario+=`<input class="materia" id="${numHorarios}${r}${c}" autocomplete="off"
+                        onkeypress="actualizarColorEscribiendo(this)"> `;           
         }
         horario+= `</div>`
     }                    
         horario += `</div>`
     horarios.innerHTML = horario;
-    crearMiniatura();    
+    //crearMiniatura();    
 }
-
+/*
 function crearMiniatura(){
     //Obtenemos la referencia a donde se colocara nuestro control de color del horario.
     let miniatura = document.getElementById("miniatura");
@@ -65,4 +67,65 @@ function actualizarColor(color,id){
     console.log(celda)
     celda.style.background= color;
 
+}*/
+
+function agregarNotacion(){
+    numNotacion++;
+    let notacion=document.getElementById("notaciones");
+    let nuevaNotacion=`
+        <div id="notacion">
+            <div class="notacion" id="notacion${numNotacion}">
+                <input type="color" id="nc${numNotacion}">
+                <input type="text" autocomplete="off" id="nt${numNotacion}">
+                <button onclick="actualizarColorBoton(this.id)" id="nb${numNotacion}">Actualizar</button>
+
+            </div>
+        </div>
+    `
+    notacion.innerHTML += nuevaNotacion;
 }
+
+
+//Actualziacion de Color con Boton Actualizar de notaciones
+function actualizarColorBoton(idInput){
+    let id=idInput.substring(2); 
+    for (let r = 7; r < 22; r++) {
+        for (let c = 0; c < 6; c++) {
+            let idCelda =`${numHorarios}${r}${c}`;
+            let celda= document.getElementById(idCelda);
+            compararCelda(celda,id);
+        }
+    }             
+}
+
+
+//Actualizacion de Color escribiendo en las celdas.
+function actualizarColorEscribiendo(celda){ 
+    for (let i = 1; i <= numNotacion; i++) {
+        compararCelda(celda,i);
+    }
+}
+
+
+
+//Compara las celdas mediante su contenido y cambia el color de las celdas.
+function compararCelda(celda,id){
+    let valor=celda.value;
+    let materia=document.getElementById("nt"+id).value;
+    let color=document.getElementById("nc"+id).value;
+    console.log(materia)
+    let array= materia.split("");
+    let arrar2=valor.split("");
+    let cadenaIgual=true;
+    for (let i = 0; i < array.length; i++) {
+        const element = array [i];
+        const element2 = arrar2[i]
+        if(element != element2){
+            cadenaIgual=false;
+        }
+    }
+    if(cadenaIgual==true){
+        celda.style.background=color;
+    }
+}
+
