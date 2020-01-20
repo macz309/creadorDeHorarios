8d369a033e714e35664a678d5b3b0ec1d768a32d
Macz309 <leugim98zurc@gmail.com>
2020-01-20 02:55:22 -0600

    Version 4.0

--- js/index.js
+++ js/index.js
@@ -25,12 +25,18 @@ function crearHorario(){
                     <p class="hora">${r}:00 - ${r+1}:00</p>`;
         for (let c = 0; c < 6; c++) {
             horario+=`<input class="materia" id="${numHorarios}${r}${c}" autocomplete="off"
-                        onkeyup="actualizarColorEscribiendo(this)"> `;           
+                        onkeyup="actualizarColorEscribiendo(this)" > `;           
         }
         horario+= `</div>`
     }                    
         horario += `</div>`
-    horarios.innerHTML = horario;   
+    horarios.innerHTML = horario;
+    let materias=document.getElementsByClassName("materia")
+    
+    for (let i = 0; i < materias.length; i++) {
+        const element = materias [i];
+        element.style.background="#ffffff";
+    }
 }
 
 //Creacion de una notacion.
@@ -46,16 +52,17 @@ function agregarNotacion(){
     notacionN.id="notacion"+numNotacion;
 
     let nc=document.createElement("input");
-    nc.addEventListener("change", function(){ actualizarColorBoton(this.id); })
     nc.type="color";
+    nc.value=colorAleatorio();
     nc.id="nc"+numNotacion;
+    nc.addEventListener("change", function(){ actualizarColorBoton(this.id); })
     notacionN.appendChild(nc);
 
     let nt=document.createElement("input");
-    nt.addEventListener("keyup", function(){ actualizarColorBoton(this.id); })
     nt.className="notacion-texto"
     nt.type="text";
     nt.id="nt"+numNotacion;
+    nt.addEventListener("keyup", function(){ actualizarColorBoton(this.id); })
     notacionN.appendChild(nt);
 
     let nb=document.createElement("button");
@@ -77,7 +84,9 @@ function actualizarColorBoton(idInput){
         for (let c = 0; c < 6; c++) {
             let idCelda =`${numHorarios}${r}${c}`;
             let celda= document.getElementById(idCelda);
-            compararCelda(celda,id);
+            if(compararCelda(celda,id,0)==true){
+                return;
+            }
         }
     }             
 }
@@ -86,39 +95,76 @@ function actualizarColorBoton(idInput){
 //Actualizacion de Color escribiendo en las celdas.
 function actualizarColorEscribiendo(celda){ 
     for (let i = 1; i <= numNotacion; i++) {
-        compararCelda(celda,i);
+        if(compararCelda(celda,i,1)==true){
+            return;
+        };
+        console.log(i)
     }
 }
 
 
 
 //Compara las celdas mediante su contenido y cambia el color de las celdas.
-function compararCelda(celda,id){
-    console.log(celda.id)
-
+function compararCelda(celda,id,tipo){
     let valor=celda.value;
     let materia=document.getElementById("nt"+id).value;
     let color=document.getElementById("nc"+id).value;
+    let rgb=hexToRgb(color);
+    //Dando formato rgb
+    let colorRGB=`rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`
     let array= materia.split("");
     let array2=valor.split("");
-
     let cadenaIgual=true;
-    if(array2[0]!=""){
-        if(array.length>0){
-            for (let i = 0; i < array.length; i++) {
-                const element = array [i];
-                const element2 = array2[i]
-                if(element != element2){
-                    cadenaIgual=false;
-                    celda.style.background="white";
-                }
+    if(array.length>0){
+        for (let i = 0; i < array.length; i++) {
+            const element = array [i];
+            const element2 = array2[i]
+            if(element != element2){
+                cadenaIgual=false;
             }
-            if(cadenaIgual==true){
-                celda.style.background=color;
-            }else {
-                celda.style.background="white";
+        }
+
+        if(cadenaIgual==true){
+            celda.style.background=color;
+            return true;
+        }else {
+            if(tipo==1){
+                celda.style.background= "#ffffff";
             }
+            return false;
+
+        }
+    }else if(tipo==0){
+        if(celda.style.backgroundColor==colorRGB){
+            celda.style.background= "#ffffff";
         }
     }
 }
 
+//Conversion de Hexadecimal a componentes RGB
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+
+  //Funcion para generar numeros aleatorios
+  function aleatorio(inferior,superior){
+    numPosibilidades = superior - inferior
+    aleat = Math.random() * numPosibilidades
+    aleat = Math.floor(aleat)
+    return parseInt(inferior) + aleat
+ }
+//Funcion para generar colores aleatorios
+  function colorAleatorio(){
+    hexadecimal = new Array("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F")
+    color_aleatorio = "#";
+    for (i=0;i<6;i++){
+       posarray = aleatorio(0,hexadecimal.length)
+       color_aleatorio += hexadecimal[posarray]
+    }
+    return color_aleatorio
+ }
