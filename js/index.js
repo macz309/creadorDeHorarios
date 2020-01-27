2f417592546421219d0e174b8c2d236e9e1187b8
macz309 <leugim98zurc@gmail.com>
2020-01-26 21:36:09 -0600

    Funcionalidad Terminada, cargar,crear y guardar horarios agregado y manejado con localStorage

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
 
 //Variables Globales.
-var numHorarios=0
 var numNotacion=0;
 var horarioActual="Inicial"
-var contenidoHorario=iniciarContenido();
 var horariosCreados=document.getElementById("horariosCreados");
+var notaciones=document.getElementById("notaciones");
+var notacionVacia=notaciones.innerHTML;
+var contenidoHorario=iniciarContenido();
 var notacionesCreadas=new Array();
 
-
 //Correr al iniciar.
-desplegarHorario()
-cargandoHorarioInicial()
+desplegarHorario();
+cargandoHorarioInicial();
 
 
 //-------------------FUNCIONES--------------------------\\
+
 //Crea un arreglo vacio equivalente a las celdas disponibles del horario.
 function iniciarContenido(){
     let contenido=[]
@@ -26,8 +27,6 @@ function iniciarContenido(){
     }
     return contenido;
 }
-
-
 
 //Creando y desplegando horario.
 function desplegarHorario(){
@@ -76,28 +75,30 @@ function desplegarHorario(){
 
 //Cargando informacio inicial del LocalStorage
 function cargandoHorarioInicial(){
-    if(localStorage.getItem("Inicial")){
-        console.log("Existe el inicial.");
+    if(localStorage.length>0){
+        for (const key in localStorage) {
+            if (localStorage.hasOwnProperty(key)) {
+                const element = localStorage[key];
+                let horarioCreado=document.createElement("option");
+                horarioCreado.innerHTML=key
+                horariosCreados.appendChild(horarioCreado);
+            }
+        }
+        //Cargando Horario "inicial"
+        cargarHorario();
     }else{
-        console.log("No existe el inicial.");
-        //localStorage.setItem(horarioActual,contenidoHorario)
         let horarioCreado=document.createElement("option");
         horarioCreado.innerHTML="Inicial"
         horariosCreados.appendChild(horarioCreado);
     }
-    console.log(localStorage.length)
     //Despliega el nombre del horario actual.
     let actual= document.getElementById("horarioActual");
     actual.value=horarioActual;
 }
 
-
-
-
 //Creacion de una notacion.
 function agregarNotacion(){
     numNotacion++;
-    let notaciones=document.getElementById("notaciones");
     //Creando notación nueva
     let notacion=document.createElement("div");
     notacion.id="notacion";
@@ -111,14 +112,14 @@ function agregarNotacion(){
     let color=colorAleatorio();
     nc.value=color;
     nc.id="nc"+numNotacion;
-    nc.addEventListener("change", function(){ actualizarColorNotacion(this); })
+    nc.addEventListener("change", function(){ actualizarColorNotacion(this) });
     notacionN.appendChild(nc);
     //Agregando Entrada de Texto
     let nt=document.createElement("input");
     nt.className="notacion-texto"
     nt.type="text";
     nt.id="nt"+numNotacion;
-    nt.addEventListener("keyup", function(){ actualizarColorNotacion(this); })
+    nt.addEventListener("keyup", function(){ actualizarColorNotacion(this) });
     notacionN.appendChild(nt);
     notacion.appendChild(notacionN);
     notaciones.appendChild(notacion);
@@ -129,13 +130,92 @@ function agregarNotacion(){
     }
     //Almacenando nuevaNotacion en las notaciones creadas.
     notacionesCreadas.push(nuevaNotacion);
-    console.log(notacionesCreadas.length);
 }
 
+//Guardar horarioActual
+function guardarHorario(){
+    let objeto={
+        horario: contenidoHorario,
+        infoNotas:notacionesCreadas,
+        notas: notaciones.innerHTML
+    };
+    localStorage.setItem(horarioActual,JSON.stringify(objeto));
+}
 
-//Actualziacion de Color con Boton Actualizar de notaciones
+//Cargar horario
+function cargarHorario(){
+    //Limpiar colores anteriores.
+    limpiarColor();
+    console.log(horariosCreados.value)
+    horarioActual=horariosCreados.value;
+    //Recuperando valores de contenidoHorario y notacionesCreadas
+    let cargado= JSON.parse(localStorage.getItem(horarioActual))
+    contenidoHorario=cargado.horario;
+    notaciones.innerHTML=cargado.notas;
+    notacionesCreadas=cargado.infoNotas;
+    numNotacion=cargado.infoNotas.length;
+    cargarNotaciones();
+    cargarContenido();
+}
+
+//Carga el contenido almacenado en el localStorage correspondientes al horario
+function cargarContenido(){
+    for (let r = 7; r < 22; r++) {
+        for (let c = 0; c < 6; c++) {
+            let idCelda =`${r}${c}`;
+            let celda= document.getElementById(idCelda);
+            celda.value=contenidoHorario[r-7][c];
+            actualizarColorEscribiendo(celda);
+        }
+    }
+}
+
+//Carga las notaciones almacenadas en el localStorage correspondientes al horario
+function cargarNotaciones(){
+    for (let i = 0; i < notacionesCreadas.length; i++) {
+        const element = notacionesCreadas[i];
+        let notacionColor = document.getElementById(`nc${i+1}`);
+        let notacionTexto = document.getElementById(`nt${i+1}`);
+        notacionColor.addEventListener("change", function(){ actualizarColorNotacion(this) });
+        notacionTexto.addEventListener("keyup", function(){ actualizarColorNotacion(this) });
+        notacionColor.value = element.color;
+        notacionTexto.value = element.texto;    
+    }
+}
+
+//Crear Horario nuevo.
+function crearNuevoHorario(){
+    numNotacion=0;
+    horarioActual=document.getElementById("nombreNuevo").value;
+    //Vaciando caja de texto.
+    document.getElementById("nombreNuevo").value="";
+    notaciones.innerHTML=notacionVacia;
+    contenidoHorario=iniciarContenido();
+    notacionesCreadas=new Array();
+    guardarHorario();
+    //Agregar nombre de horario en el selector de horarios
+    let horarioCreado=document.createElement("option");
+        horarioCreado.innerHTML=horarioActual;
+        horariosCreados.appendChild(horarioCreado);
+    //Despliega el nombre del horario actual.
+    let actual= document.getElementById("horarioActual");
+    actual.value=horarioActual;
+    //Cargar contenido en blanco
+    cargarContenido();
+    //Limpiar colores anteriores.
+    limpiarColor();
+}
+
+//Actualziacion de Color de notaciones
 function actualizarColorNotacion(input){
     let id=input.id.substring(2); 
+    //Actualizacion de datos almacenados en notacionesCreadas.
+    if(input.type=="color"){
+        notacionesCreadas[id-1].color=input.value;
+    }else{
+        notacionesCreadas[id-1].texto=input.value;
+    }
+    //Actualizar Color en celdas
     for (let r = 7; r < 22; r++) {
         for (let c = 0; c < 6; c++) {
             let idCelda =`${r}${c}`;
@@ -145,23 +225,16 @@ function actualizarColorNotacion(input){
             }
         }
     }
-    //Actualizacion de datos almacenados en notacionesCreadas.
-    if(input.type=="color"){
-        notacionesCreadas[id-1].color=input.value;
-    }else{
-        notacionesCreadas[id-1].texto=input.value;
-    }
-    console.log(notacionesCreadas);
 }
 
 //Actualizacion de Color escribiendo en las celdas.
 function actualizarColorEscribiendo(celda){ 
+    actualizarContenidoHorario(celda.id,celda.value);
     for (let i = 1; i <= numNotacion; i++) {
         if(compararCelda(celda,i,1)==true){
             return;
         };
     }
-    actualizarContenidoHorario(celda.id,celda.value);
 }
 
 //Actualizar contenidoHorario
@@ -214,6 +287,16 @@ function compararCelda(celda,id,tipo){
     }
 }
 
+
+//Limpiar color de las celdas
+function limpiarColor(){
+    let dias=document.getElementsByClassName("materias")
+    for (let i = 0; i < dias.length; i++) {
+        const element = dias[i];
+        element.style.background="#ffffff";
+        
+    }
+}
 //Conversion de Hexadecimal a componentes RGB
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
